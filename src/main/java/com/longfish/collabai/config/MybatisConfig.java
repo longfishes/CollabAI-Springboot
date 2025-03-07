ac86fb2d7e09594e0d391290dce055eb95d5fb3c
longfish <longfishes@qq.com>
2025-03-07 15:31:11 +0800

    清理代码

--- src/main/java/com/longfish/collabai/config/MybatisConfig.java
+++ src/main/java/com/longfish/collabai/config/MybatisConfig.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class MyBatisConfig {
+public class MybatisConfig {
 
     @Value("${page.max}")
     private Long limit;
