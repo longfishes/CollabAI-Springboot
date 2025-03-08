8cc59232641724d65e2dc716b6e87899bd42e1d6
longfish <longfishes@qq.com>
2025-03-08 18:15:56 +0800

    语音识别消费者 内容同步数据库

--- src/main/java/com/longfish/collabai/consumer/RecognizeConsumer.java
+++ src/main/java/com/longfish/collabai/consumer/RecognizeConsumer.java
@@ -1,10 +1,14 @@
 package com.longfish.collabai.consumer;
 
 import com.alibaba.fastjson.JSON;
+import com.longfish.collabai.exception.BizException;
 import com.longfish.collabai.pojo.dto.RecognizeDTO;
+import com.longfish.collabai.pojo.entity.Meeting;
+import com.longfish.collabai.service.IMeetingService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import static com.longfish.collabai.constant.RabbitMQConstant.RECOGNIZE_QUEUE;
@@ -14,10 +18,18 @@ import static com.longfish.collabai.constant.RabbitMQConstant.RECOGNIZE_QUEUE;
 @RabbitListener(queues = RECOGNIZE_QUEUE)
 public class RecognizeConsumer {
 
+    @Autowired
+    private IMeetingService meetingService;
+
     @RabbitHandler
     public void process(byte[] data) {
         RecognizeDTO recognizeDTO = JSON.parseObject(new String(data), RecognizeDTO.class);
-        log.debug(recognizeDTO.getContent());
+        Meeting meeting = meetingService.getById(recognizeDTO.getMeetingId());
+        if (meeting == null) {
+            throw new BizException("会议号不存在");
+        }
+        meeting.setSpeechText(meeting.getSpeechText() + "\n" + recognizeDTO.getContent());
+        meetingService.updateById(meeting);
     }
 
 }
